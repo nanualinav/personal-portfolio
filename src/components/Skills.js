7efaf376cb36c551b39101e111d21fada1feb3ea
Alina Nanu <nanualinav@gmail.com>
2023-04-28 00:51:33 +0300

    Projects section implementation

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -1,11 +1,12 @@
 import React from 'react'
-import "react-multi-carousel/lib/styles.css";
-import Carousel from "react-multi-carousel";
 import { Col, Container, Row } from 'react-bootstrap';
+import Carousel from "react-multi-carousel";
+import "react-multi-carousel/lib/styles.css";
 
 import meter1 from '../assets/img/meter1.svg';
 import meter2 from '../assets/img/meter2.svg';
 import meter3 from '../assets/img/meter3.svg';
+import colorSharp from "../assets/img/banner-bg.jpg"
 
 export const Skills = () => {
     const responsive = {
@@ -58,8 +59,7 @@ export const Skills = () => {
             </Col>
         </Row>
       </Container>
+      <img className="background-image-left" src={colorSharp} alt="Image" />
     </section>
   )
 }
-
-export default Skills
