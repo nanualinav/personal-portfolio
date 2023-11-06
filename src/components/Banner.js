4e4f494c164cc176d81dcf050ae99b740424eefe
Alina Nanu <nanualinav@gmail.com>
2023-11-06 12:51:58 +0200

    Banner section refactoring

--- src/components/Banner.js
+++ src/components/Banner.js
@@ -1,75 +1,65 @@
-import React, { useEffect, useState } from 'react';
-import { Col, Container, Row } from 'react-bootstrap';
-import { ArrowRightCircle } from 'react-bootstrap-icons';
-import headerImg from '../assets/img/header-img.svg'; 
+import { Col, Container, Row } from "react-bootstrap";
+import headerImg from "../assets/img/header-img.svg";
+import linkedinIcon from "../assets/img/linkedin-icon.svg";
+import githubIcon from "../assets/img/github-icon.svg";
+import behanceIcon from "../assets/img/behance-icon.svg";
+import dribbleIcon from "../assets/img/dribbble-icon.svg";
 
 export const Banner = () => {
-    const [loopNo, setLoopNo] = useState(0);
-    const [isDeleting, setIsDeleting] = useState(false);
-    const [text, setText] = useState('');
-    const [delta, setDelta] = useState(200 - Math.random() * 100);
-    const [, setIndex] = useState(1);
-    const toRotate = ['Front-End Developer', 'iOS Developer'];
-    const period = 2000;
-
-    useEffect(() => {
-        const ticker = setInterval(() => {
-            tick()
-        }, delta)
-
-        return () => { clearInterval(ticker) }
-    });
-
-    const handleOnConnect = () => {
-        window.scrollTo({
-            top: document.documentElement.scrollHeight,
-            behavior: 'smooth',
-          });
-    };
-
-    const tick = () => {
-        let i = loopNo % toRotate.length;
-        let fullText = toRotate[i];
-        let updatedText = isDeleting ? fullText.substring(0, text.length - 1) : fullText.substring(0, text.length + 1)
-
-        setText(updatedText)
-
-        if (isDeleting) {
-            setDelta(prevDelta => prevDelta / 2)
-        }
-
-        if (!isDeleting && updatedText === fullText) {
-            setIsDeleting(true)
-            setIndex(prevIndex => prevIndex - 1)
-            setDelta(period);
-        } else if (isDeleting && updatedText === '') {
-            setIsDeleting(false)
-            setLoopNo(loopNo + 1)
-            setIndex(1)
-            setDelta(300)
-        } else {
-            setIndex(prevIndex => prevIndex + 1)
-        }
-    }
-
   return (
-    <section className='banner' id='home'>
+    <section className="banner" id="home">
       <Container>
-        <Row className='align-items-center'>
-            <Col xs={12} md={6} xl={7}>
-                <span className='tagline'>Welcome to my Portfolio</span>
-                <h1>{`Hi! My name is Alina, `}<br/> <span className='wrap'>{text}</span></h1>
-                <br/>
-                <p>Experienced iOS and Front-End Developer with a strong passion for crafting elegant and efficient solutions. With my hands-on experience in iOS app development, I have a deep understanding of Swift and UIKit, along with expertise in integrating APIs and implementing sleek user interfaces.</p>
-                <p>Additionally, I have recently ventured into web development, where I have gained valuable skills in HTML, CSS, and JavaScript, along with  React framework.</p>
-                <button onClick={handleOnConnect}>Let's connect <ArrowRightCircle size={25}/></button>
-            </Col>
-            <Col xs={12} md={6} xl={5}>
-                <img src={headerImg} alt='Header Img'/>
-            </Col>
+        <Row className="align-items-center">
+          <Col xs={12} md={6} xl={7}>
+            <h2>Hi! I'm Alina,</h2>
+            <h1 className="mb-3">Software Engineer</h1>
+            <div className="social-icon">
+              <a
+                className="me-2"
+                href="https://www.linkedin.com/in/alina-nanu/"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={linkedinIcon} alt="" />
+              </a>
+              <a
+                className="me-2"
+                href="https://github.com/nanualinav"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={githubIcon} alt="" />
+              </a>
+              <a
+                className="me-2"
+                href="https://www.behance.net/nanualina"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={behanceIcon} alt="" />
+              </a>
+              {/* <a
+                href="https://dribbble.com/nanualinav"
+                target="_blank"
+                rel="noreferrer"
+              >
+                <img src={dribbleIcon} alt="" />
+              </a> */}
+            </div>
+            <br />
+            <br />
+            <p>
+              🖥️ I love exploring and creating. My goal is to blend coding
+              expertise with the passion for design and deliver captivating and
+              interactive web experiences.
+            </p>
+          </Col>
+          <Col xs={12} md={6} xl={5}>
+            <img src={headerImg} className="headerImg" alt="Header Img" />
+          </Col>
         </Row>
       </Container>
-      <div className='transition'></div>
+      <div className="transition"></div>
     </section>
-  )
-}
+  );
+};
