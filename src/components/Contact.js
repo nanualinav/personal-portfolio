199b1a280d223dc59a9c876078e7f389b26bf52f
Alina Nanu <nanualinav@gmail.com>
2023-11-06 12:52:10 +0200

    Contact section refactoring

--- src/components/Contact.js
+++ src/components/Contact.js
@@ -1,31 +1,30 @@
-import React from 'react'
-import { Col, Container, Row } from 'react-bootstrap';
-import TrackVisibility from 'react-on-screen';
-import 'animate.css';
+import React from "react";
+import { Col, Container } from "react-bootstrap";
+import "animate.css";
 
-import contactImg from '../assets/img/contact-me.svg'
+import coffeeImg from "../assets/img/coffeeImg.png";
 
 export const Contact = () => {
   return (
-    <section className='contact' id='connect'>
-        <Container>
-            <Row className='align-items-center'>
-                <Col size={9} md={6}>
-                    <TrackVisibility>
-                        {({ isVisible}) => 
-                            <img src={contactImg} alt='Contact Me'/>}
-                    </TrackVisibility>
-                </Col>
-                <Col md={6}>
-                    <form >     
-                        <Row>
-                        <button type='submit' onClick={()=> window.location.href='mailto:nanualinav@gmail.com'}><span>Send me an email</span></button>
-                        </Row>
-                    </form>
-                </Col>
-            </Row>
-        </Container>
-      
+    <section className="contact" id="connect">
+      <Container className="d-flex justify-content-center">
+        <div className="mb-5">
+          <h1>Let's have a chat</h1>
+          <p>nanualinav@gmail.com</p>
+          <img className="mr-5" src={coffeeImg} alt="Contact Me" />
+          <Col>
+          <button
+          className="mt-4"
+            type="submit"
+            onClick={() =>
+              (window.location.href = "mailto:nanualinav@gmail.com")
+            }
+          >
+            <span>Send me an email</span>
+          </button>
+          </Col>  
+        </div>
+      </Container>
     </section>
-  )
-}
+  );
+};
